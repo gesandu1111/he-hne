3ded66180e8d3491a6b15274ca38616e398ec118
gesandu1111 <gesandusanmira@gmail.com>
2026-01-02 03:21:43 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,11 +1,31 @@
 const { Storage } = require("megajs");
 
+// Use your Mega account email & password
+const MEGA_EMAIL = "gesandsanmira19202@gmail.com";
+const MEGA_PASSWORD = "Gesa123!@#";
+
 async function upload(stream, filename) {
   return new Promise((resolve, reject) => {
-    const file = new Storage.File({ name: filename });
-    file.upload(stream, (err) => {
-      if (err) return reject(err);
+    // Create Mega storage instance
+    const storage = new Storage({
+      email: MEGA_EMAIL,
+      password: MEGA_PASSWORD,
+    });
+
+    const file = storage.upload({
+      name: filename,
+    });
+
+    // Pipe the stream
+    stream.pipe(file);
+
+    file.on("ready", () => {
+      // Get download link
       resolve(file.link());
+    });
+
+    file.on("error", (err) => {
+      reject(err);
     });
   });
 }
